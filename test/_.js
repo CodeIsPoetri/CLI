f3346610995cdfc324ab690f83360e9acaf89743
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2018-11-28 16:40:53 -0500

    Tests for lib/auth.api.js

--- test/_.js
+++ test/_.js
@@ -1,1 +1,9 @@
 'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+
+global.expect = chai.expect;
+global.Agent = require('supertest-as-promised');
