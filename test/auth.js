8023d8fe64ad7500c4e88a5cf539620f48c80552
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2018-11-28 16:40:53 -0500

    Tests for lib/auth.api.js

---
+++ test/auth.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const AuthApi = require('../lib/auth.api');
+const Token = require('../lib/token');
+
+describe('Auth', () => {
+    const user = {
+        username: 'jdoe',
+        firstName: 'John',
+        lastName: 'Doe',
+        mail: 'doe@example.com',
+        password: 'im-john-doe',
+        sshPublicKey: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQHG8c9zz8ye6XQO0u+4TnscuQq+8P5lFx7d5lLqZ6vIw2O76EgDQpoMgaWJwZeRZGviafbVFeqtKgmaku68U2yJ+e/k+8IGYMf3fqL8aR+tSz2Tx0SCjlPRk+iTWVihspbhWk2X16CdFxuwFjGvqyHC7VkoyBWL/MQ/C8HeMfHsf5XzVHww53cQikLfEENxXsNw8ExHha9VLLTiLmc6grPO5tmyAadFLFhPKDSWcxT5JabCQiWSSpoOWDUmdwIfoomEKB64pG76XXVLLjcXC7yoaiiqvCix+4uJ+vSdXIbc3ouE3aPHJhJKpAxyeLgvygphUfB/wPdAmSnwo8OKmV'
+    };
+
+    it('#register', () => expect(AuthApi.register(user))
+        .to.eventually.be.fulfilled);
+
+    it('#login', async () => {
+        await AuthApi.login(user.mail, user.password);
+        return expect(Token.get()).to.be.ok;
+    });
+});
