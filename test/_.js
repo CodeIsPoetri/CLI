29ad70483fdab408d23464bf2fede6105f0babda
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2019-01-16 22:10:51 -0500

    Update dependencies

--- test/_.js
+++ test/_.js
@@ -6,4 +6,4 @@ const chai = require('chai');
 chai.use(require('chai-as-promised'));
 
 global.expect = chai.expect;
-global.Agent = require('supertest-as-promised');
+global.Agent = require('supertest');
